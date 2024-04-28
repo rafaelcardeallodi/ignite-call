a476a7b0783225e959ba48db5f6bb89ecc6ad982
rafaelcardeallodi <rafaelcardeallodi@hotmail.com>
2024-04-28 20:57:07 -0300

    Configurando o Next Auth

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,18 +1,24 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { SessionProvider } from 'next-auth/react'
 
 import { globalStyles } from '@/styles/global'
 
 globalStyles()
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
-    <main>
+    <>
       <Head>
         <title>Ignite Call</title>
       </Head>
 
-      <Component {...pageProps} />
-    </main>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   )
 }
